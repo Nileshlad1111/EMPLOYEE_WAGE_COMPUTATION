ac5222df5067defd0b161d56840efb67f579a90f
Nileshlad1111 <nileshcareer1111@gmail.com>
2021-05-05 15:33:30 +0530

    Added: To calculate daily employee wage

--- EmployeeWageCompution.sh
+++ EmployeeWageCompution.sh
@@ -1,3 +1,7 @@
+#!/bin/bash -x
+
+echo "------------------------- Welcome To Employee Wage Compution ------------------------"
+
 #INITIAL VALUE TO ASSING PRESENT
 isPresent=1
 
@@ -11,3 +15,12 @@ then
 else
 	echo "Employee Absent"
 fi
+
+#TO FUNCTION CALCULATE EMPLOYEE WAGE
+function calculateDailyEmployeeWage(){
+	read -p "Assume Wage per hour is :" isWagePerHour "rupees"
+	read -p "To full day hours is :" isFullDayHour
+	calculateEmployeeWage=$(( $isWagePerHour * $isFullDayHour ))
+	echo "To calculate daily employee wage :" $calculateEmployeeWage "rupees"
+}
+calculateDailyEmployeeWage
