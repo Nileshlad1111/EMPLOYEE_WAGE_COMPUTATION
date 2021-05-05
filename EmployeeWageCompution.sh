69d79682c51bbb489b5ba4eac03ff0d52d197b9c
Nileshlad1111 <nileshcareer1111@gmail.com>
2021-05-05 08:58:05 +0530

    Added:To check the emplyee present and absent

--- EmployeeWageCompution.sh
+++ EmployeeWageCompution.sh
@@ -1,1 +1,13 @@
-----------------------welcome Employee wage compution--------------------------
+#INITIAL VALUE TO ASSING PRESENT
+isPresent=1
+
+#TO VARIABLE
+randomNumber=$((RANDOM%2))
+
+#TO CHECK THE EMPLOYEE PRESENT AND ABSENT
+if [ $randomNumber -eq 	$isPresent ]
+then
+	echo "Employee Present"
+else
+	echo "Employee Absent"
+fi
